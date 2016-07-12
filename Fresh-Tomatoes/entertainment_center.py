f01a08c0923cb14bfa25e025303d13b9d980436b
Sandy O <sandrine10@users.noreply.github.com>
2016-07-12 07:52:31 -0400

    initial commit

---
+++ Fresh-Tomatoes/entertainment_center.py
@@ -0,0 +1,262 @@
+import fresh_tomatoes
+import media
+
+
+inception = media.Movie("Inception(2010)",
+                        "Dom Cobb is a thief with the rare ability to enter people's"
+                        "dreams and steal their secrets from their subconscious",
+                        "Cobb, Arthur, Ariadne",
+                        "PG-13",
+                        "https://upload.wikimedia.org/wikipedia/en/thumb/7/7f/Inception_ver3.jpg/220px-Inception_ver3.jpg",
+                        "http://www.imdb.com/title/tt1375666/?ref_=fn_al_tt_1",
+                        "https://www.youtube.com/watch?v=8hP9D6kZseM")
+
+slumdog_millionaire = media.Movie ("Slumdog Millionaire(2008)",
+                                   "Jamal Malik from the Juhu Slum, is a contestant on the"
+                                   "Indian version of Who Wants to Be a Millionaire?",
+                                   "Jamal, Latika, Salim",
+                                   "R",
+                                   "http://i5.walmartimages.com/dfw/dce07b8c-4472/k2-_44b04fb0-ecce-460b-b1be-b89a429aebf3.v2.jpg",
+                                   "http://www.imdb.com/title/tt1010048/?ref_=nv_sr_1",
+                                   "https://www.youtube.com/watch?v=kuSTr48P9mc")
+
+blood_diamond = media.Movie ("Blood Diamond(2006)",
+                             "It is 1999 and Sierra Leone is ravaged by major political unrest",
+                             "Danny, Solomon, Maddy",
+                             "R",
+                             "https://upload.wikimedia.org/wikipedia/en/5/5a/Blooddiamondposter.jpg",
+                             "http://www.imdb.com/title/tt0450259/?ref_=nv_sr_6",
+                             "https://www.youtube.com/watch?v=-AerMQf5JCM")
+
+pretty_woman = media.Movie ("Pretty Woman(1990)",
+                            "In this modern update of cinderella, a prostitute and a wealthy"
+                            "businessman fall hard for one another forming an unlikely pair",
+                            "Edward, Vivian, Philipp",
+                            "R",
+                            "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Pretty_woman_movie.jpg/220px-Pretty_woman_movie.jpg",
+                            "http://www.imdb.com/title/tt0100405/?ref_=nv_sr_2",
+                            "https://www.youtube.com/watch?v=MAiQvt1_TsM")
+
+the_matrix = media.Movie ("The Matrix(1999)",
+                          "An infamous hacker called Trinity is cornered in an abandoned hotel by police",
+                          "Neo, Morpheus, Trinity",
+                          "R",
+                          "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
+                          "http://www.imdb.com/title/tt0133093/?ref_=nv_sr_1",
+                          "https://www.youtube.com/watch?v=vKQi3bBA1y8")
+
+the_notebook = media.Movie ("The Notebook(2004)",
+                            "In 1940s South Carolina, mill worker Noah Calhoun and rich girl Allie"
+                            "are desperately in love",
+                            "Noah, Allie, Duke",
+                            "PG-13",
+                            "https://upload.wikimedia.org/wikipedia/en/thumb/8/86/Posternotebook.jpg/215px-Posternotebook.jpg",
+                            "http://www.imdb.com/title/tt0332280/?ref_=nv_sr_1",
+                            "https://www.youtube.com/watch?v=uS98235uw5I")
+
+the_bodyguard = media.Movie ("The Bodyguard(1992)",
+                             "Best-selling pop diva Rachel Marron has a stalker whose obsession has risen to the level of"
+                             "disturbing threats",
+                             "Frank, Rachel",
+                             "R",
+                             "https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/The_Bodyguard_1992_Film_Poster.jpg/220px-The_Bodyguard_1992_Film_Poster.jpg",
+                             "http://www.imdb.com/title/tt0103855/",
+                             "https://www.youtube.com/watch?v=4JFRdJTszRM")
+
+there_is_something_about_mary = media.Movie ("There Is Something About Mary(1998)",
+                                             "A man gets a chance to meet up with his dream girl from highschool, even though his date with her was a disaster",
+                                             "Mary, Healy, Ted",
+                                             "R",
+                                             "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/There%27s_Something_About_Mary_POSTER.jpg/220px-There%27s_Something_About_Mary_POSTER.jpg",
+                                             "http://www.imdb.com/title/tt0129387/?ref_=fn_al_tt_1",
+                                             "https://www.youtube.com/watch?v=eGjXwDYpOLE")
+
+catch_me_if_you_can = media.Movie ("Catch Me If You Can(2002)",
+                                   "A true story about Frank Abagnale Jr, who, before his 19th birthday, successfully conned millions of dollar's worth of checks as a Pam Am pilot, doctor and"
+                                   "legal prosecutor",
+                                   "Frank Jr, Carl, Frank, Paula, Roger, Brenda",
+                                   "PG-13",
+                                   "https://upload.wikimedia.org/wikipedia/en/thumb/4/4d/Catch_Me_If_You_Can_2002_movie.jpg/220px-Catch_Me_If_You_Can_2002_movie.jpg",
+                                   "http://www.imdb.com/title/tt0264464/?ref_=rvi_tt",
+                                   "https://www.youtube.com/watch?v=SosRcIMCr5g")
+
+the_wolverine = media.Movie ("The Wolverine(2013)",
+                             "When Wolverine is summoned to Japan by an old acquaintance, he is embroiled in a conflict that forces him to confort his own demons",
+                             "logan, Mariko, Yukio, Shingen",
+                             "PG-13",
+                             "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/The_Wolverine_posterUS.jpg/220px-The_Wolverine_posterUS.jpg",
+                             "http://www.imdb.com/title/tt1430132/?ref_=nv_sr_1",
+                             "https://www.youtube.com/watch?v=Rh1LdTFkm7I")
+
+avatar = media.Movie ("Avatar(2009)",
+                      "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home",
+                      "Jack, Neytiri, Dr.Grace Augustine, Colonel Miles Quaritch, Trudy",
+                      "PG-13",
+                      "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg",
+                      "http://www.imdb.com/title/tt0499549/?ref_=nv_sr_1",
+                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+gravity = media.Movie ("Gravity(2013)",
+                       "A medical engineer and an astronaut work together to survive after an accident leaves them adrift in space",
+                       "Ryan, Matt",
+                       "PG-13",
+                       "https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Gravity_Poster.jpg/220px-Gravity_Poster.jpg",
+                       "http://www.imdb.com/title/tt1454468/?ref_=nv_sr_1",
+                       "https://www.youtube.com/watch?v=_KJHRF6RlTQ")
+
+duplicity = media.Movie ("Duplicity(2009)",
+                         "Two ex government agents turned rival industrial spies have to be at the top of their game when one of their companies prepares to launch a major product",
+                         "Ray, Claire, Howard",
+                         "PG-13",
+                         "https://upload.wikimedia.org/wikipedia/en/thumb/0/09/Duplicity_film.jpg/220px-Duplicity_film.jpg",
+                         "http://www.imdb.com/title/tt1135487/?ref_=fn_al_tt_1",
+                         "https://www.youtube.com/watch?v=S8R3SZqQpJI")
+
+oblivion = media.Movie ("Oblivion(2013)",
+                        "A veteran assigned to extract Earth's remaining resources begins to question what he knows about his mission and himself",
+                        "Jack, Beech, Julia",
+                        "PG-13",
+                        "https://upload.wikimedia.org/wikipedia/en/thumb/2/2e/Oblivion2013Poster.jpg/220px-Oblivion2013Poster.jpg",
+                        "http://www.imdb.com/title/tt1483013/?ref_=nv_sr_1",
+                        "https://www.youtube.com/watch?v=vGpjlfCfe2Y")
+
+the_wolf_of_wall_street = media.Movie("The Wolf Of Wall Street(2013)",
+                                      "Based on the true stroy of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime",
+                                      "Jordan, Donnie,Naomi, Mark",
+                                      "R",
+                                      "https://upload.wikimedia.org/wikipedia/en/thumb/1/1f/WallStreet2013poster.jpg/220px-WallStreet2013poster.jpg",
+                                      "http://www.imdb.com/title/tt0993846/?ref_=nv_sr_1",
+                                      "https://www.youtube.com/watch?v=dFXFyNoNq3A")
+
+the_departed = media.Movie ("The Departed(2006)",
+                            "An undercover cop and a mole in the police attempt to identify each other while infiltrating an irish gang in South Boston",
+                            "Billy, Colin, Frank, Dignam, Queenan",
+                            "R",
+                            "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Departed234.jpg/220px-Departed234.jpg",
+                            "http://www.imdb.com/title/tt0407887/?ref_=nv_sr_1",
+                            "https://www.youtube.com/watch?v=auYbpnEwBBg")
+
+basic_instinct = media.Movie ("Basic Instinct(1992)",
+                              "A violent, suspended police detective investigates a brutal murder, in which a seductive woman could be involved",
+                              "detective Nick Curran, Catherine, Gus,",
+                              "R",
+                              "https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/Basic_instinct.jpg/220px-Basic_instinct.jpg",
+                              "http://www.imdb.com/title/tt0103772/?ref_=nv_sr_1",
+                              "https://www.youtube.com/watch?v=wLGx5YT6sOg")
+
+finding_nemo = media.Movie ("Finding Nemo(2003)",
+                            "After his son is captured in the Great Barrier reef and taken to Sydney, a timid clownfish sets out on a journey to bring him home",
+                            "Nemo, Marlin, Dory, Gill",
+                            "G",
+                            "https://upload.wikimedia.org/wikipedia/en/thumb/2/29/Finding_Nemo.jpg/220px-Finding_Nemo.jpg",
+                            "http://www.imdb.com/title/tt0266543/?ref_=nv_sr_1",
+                            "https://www.youtube.com/watch?v=JTZd2l8qBOw")
+
+the_aviator = media.Movie ("The Aviator(2004)",
+                           "In Houston, 1913, nine-year-old Howard Hughes is warned by his mother of the diseases to which she is afraid he will succumb",
+                           "Howard, Katharine, Ava, Noah, Juan",
+                           "PG-13",
+                           "https://upload.wikimedia.org/wikipedia/en/thumb/f/f7/The_Aviator_Poster.jpg/220px-The_Aviator_Poster.jpg",
+                           "http://www.imdb.com/title/tt0338751/?ref_=rvi_tt",
+                           "https://www.youtube.com/watch?v=FebPJlmgldE")
+
+
+dirty_dancing = media.Movie ("Dirty Dancing(1987)",
+                             "Spending the summer at a Catskills resort with her famili, Frances Houseman falls in love with the camp's dance instructor, Johnny Castle",
+                             "Baby, Johnny, Jake, Penny",
+                             "PG-13",
+                             "https://upload.wikimedia.org/wikipedia/en/thumb/0/00/Dirty_Dancing.jpg/220px-Dirty_Dancing.jpg",
+                             "http://www.imdb.com/title/tt0092890/?ref_=nv_sr_2",
+                             "https://www.youtube.com/watch?v=eIcmQNy9FsM")
+
+the_last_of_the_mohicans = media.Movie("The Last Of The Mohicans(1992)",
+                                       "Three trappers protect a British Colonel's daughters in themidst of the French and Indian War.", 
+                                       "Hawkeye, Cora, Chingachgook",
+                                       "R",
+                                       "https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Mohicansposter.jpg/220px-Mohicansposter.jpg",
+                                       "http://www.imdb.com/title/tt0104691/?ref_=nv_sr_1",
+                                       "https://www.youtube.com/watch?v=yaQeVnN6pUc")
+
+gran_torino = media.Movie ("Gran Torino(2008)",
+                           "Disgruntled Korean War veteran Walt Kowalski sets out to reform his neighbor, a Hmong teenager who tried to steal Kowalski's prized possession"
+                           "a 1972 Gran Torino.",
+                           "Walt Kowalski, Father Janovich, Thao, Sue,",
+                           "R",
+                           "https://upload.wikimedia.org/wikipedia/en/thumb/c/c6/Gran_Torino_poster.jpg/220px-Gran_Torino_poster.jpg",
+                           "http://www.imdb.com/title/tt1205489/?ref_=nv_sr_1",
+                           "https://www.youtube.com/watch?v=RMhbr2XQblk")
+
+million_dollar_baby = media.Movie ("Million Dollar Baby(2004)",
+                                   "A determined woman works with a hardened boxing trainer to become a professional.",
+                                   "Frankie, Maggie, Eddie",
+                                   "PG-13",
+                                   "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Million_Dollar_Baby_poster.jpg/220px-Million_Dollar_Baby_poster.jpg",
+                                   "http://www.imdb.com/title/tt0405159/?ref_=tt_rec_tt",
+                                   "https://www.youtube.com/watch?v=5_RsHRmIRBY")
+
+skyfall = media.Movie ("Skyfall(2012)",
+                       "Bond's loyalty to M is tested when her past comes back to haunt her.",
+                       "James Bond, M, Silva",
+                       "PG-13",
+                       "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Skyfall_poster.jpg/220px-Skyfall_poster.jpg",
+                       "http://www.imdb.com/title/tt1074638/?ref_=nv_sr_1",
+                       "https://www.youtube.com/watch?v=6kw1UVovByw")
+
+the_master = media.Movie ("The Master(2012)",
+            "A naval veteran arrives home from war unsettled and uncertain of his future, until he is tantalized by The Cause and its charismatic leader",
+            "Freddie, V.A. Doctor",
+            "R",
+            "https://upload.wikimedia.org/wikipedia/en/thumb/0/08/TheMaster2012Poster.jpg/220px-TheMaster2012Poster.jpg",
+            "http://www.imdb.com/title/tt1560747/?ref_=nv_sr_1",
+            "https://www.youtube.com/watch?v=xENEchTFk-E")
+
+the_bucket_list = media.Movie ("The Bucket List(2007)",
+                               "Two terminally ill men escape from a cancer ward and head off on a road trip with a wish list of to-dos before they die.",
+                               "Edward, carter, Thomas, Virginia",
+                               "PG-13",
+                               "https://upload.wikimedia.org/wikipedia/en/thumb/2/20/Bucket_list_poster.jpg/220px-Bucket_list_poster.jpg",
+                               "http://www.imdb.com/title/tt0825232/?ref_=nm_flmg_act_3",
+                               "https://www.youtube.com/watch?v=UvdTpywTmQg")
+
+inside_man = media.Movie ("Inside Man(2006)",
+                          "A police detective, a bank robber, and a high-power broker enter high-stakes negotiations after the criminal's brilliant heist spirals into a"
+                          "hostage situation",
+                          "Detective Frazier, Dalton, Madeleine, Arthur",
+                          "R",
+                          "https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/Inside_Man_%28film_poster%29.jpg/220px-Inside_Man_%28film_poster%29.jpg",
+                          "http://www.imdb.com/title/tt0454848/?ref_=nm_knf_t4",
+                          "https://www.youtube.com/watch?v=jnkS0XIePF8")
+
+bruce_almighty = media.Movie ("Bruce Almighty(2003)",
+                              "A guy who complains about God too often is given almighty powers to teach him how difficult it is to run the world.",
+                              "Bruce, God, Grace",
+                              "PG-13",
+                              "https://upload.wikimedia.org/wikipedia/en/6/60/BruceAlmighty_poster.jpg",
+                              "http://www.imdb.com/title/tt0315327/?ref_=nm_flmg_act_25",
+                              "https://www.youtube.com/watch?v=fe-luzrqWSk")
+
+the_vow = media.Movie ("The Vow(2012)",
+                       "A car accident puts paige in a coma, and when she wakes up with severe memory loss, her husband Leo works to win her heart again",
+                       "Paige, Leo, Rita, Bill",
+                       "PG-13",
+                       "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/The_Vow_Poster.jpg/220px-The_Vow_Poster.jpg",
+                       "http://www.imdb.com/title/tt1606389/",
+                       "https://www.youtube.com/watch?v=PcL24s-S6ns")
+
+ps_I_love_you = media.Movie ("P.S. I Iove You(2007)",
+                              "A young widow discovers that her late husband has left her 10 messages intended to help ease her pain and start a new life.",
+                              "Holly, Gerry, Denise, Sharon",
+                              "PG-13",
+                              "https://upload.wikimedia.org/wikipedia/en/thumb/7/7f/PS_I_Love_You_%28film%29.jpg/220px-PS_I_Love_You_%28film%29.jpg",
+                              "http://www.imdb.com/title/tt0431308/?ref_=tt_rec_tti",
+                              "https://www.youtube.com/watch?v=1rqqidmUmSk")
+
+                       
+
+                            
+movies = [inception, slumdog_millionaire, blood_diamond, pretty_woman, the_matrix, the_notebook, the_bodyguard, there_is_something_about_mary, catch_me_if_you_can,
+          the_wolverine,avatar, gravity, duplicity, oblivion, the_wolf_of_wall_street, the_departed, basic_instinct, finding_nemo, the_aviator, dirty_dancing,
+          the_last_of_the_mohicans, gran_torino, million_dollar_baby, skyfall, the_master, the_bucket_list, inside_man, bruce_almighty, the_vow, ps_I_love_you]
+fresh_tomatoes.open_movies_page(movies)
+
+
